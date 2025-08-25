422947ba6fa9dfbf15ec0f991c2303f3b4ad63ed
Murphy Tian <murphytian06@gmail.com>
2025-08-25 13:55:57 +0000

    remove: delete unused server implementations for HybridInference

--- serving/servers/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Server implementations for HybridInference.
-
-This module contains server-side implementations that provide APIs,
-as opposed to client implementations in serving/providers/.
-"""
-
-from .openrouter import app as openrouter_app
-
-__all__ = ["openrouter_app"]
