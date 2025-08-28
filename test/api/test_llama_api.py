cd343d700bf33e88321d8ef2f9a6f0eeeb8e1365
Murphy Tian <murphytian06@gmail.com>
2025-08-28 12:47:03 +0000

    test: test llama3.3 70b instruct api

--- test/api/test_llama_api.py
+++ test/api/test_llama_api.py
@@ -9,10 +9,25 @@ client = OpenAI(
     api_key=os.environ["LLAMA_API_KEY"],
     base_url="https://api.llama.com/compat/v1/"
 )
+print(os.environ["LLAMA_API_KEY"])
 
 # Create chat completion request
 completion = client.chat.completions.create(
     model="Llama-3.3-8B-Instruct",
+    messages=[
+        {
+            "role": "developer",
+            "content": "You are a helpful assistant."
+        },
+        {
+            "role": "user",
+            "content": "Hello!"
+        }
+    ],
+)
+
+completion= client.chat.completions.create(
+    model="Llama-3.3-70B-Instruct",
     messages=[
         {
             "role": "developer",
